e1c8fc7b31021b3bc0b6506ebc29284dd09da8ec
jamiroal <jamiroal>
2021-03-05 11:03:01 -0500

    feat(mobile menu): adding mobile menu section and basic functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,9 @@
 let upButton = document.getElementById('go-up');
 
 const getTopPosition = () => {
-  document.body.getBoundingClientRect().top < 0
-    ? (upButton.style.display = 'block')
-    : (upButton.style.display = 'none');
+  document.body.getBoundingClientRect().top === 0
+    ? (upButton.style.display = 'none')
+    : (upButton.style.display = 'block');
 };
 
 window.onscroll = (e) => {
@@ -15,4 +15,29 @@ window.onscroll = (e) => {
 upButton.addEventListener('click', () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+//Mobile menu functionalities
+
+const mobileMenuClick = document.querySelector('.mobile-menu'),
+  mobileMenuIconClick = mobileMenuClick.firstElementChild,
+  mobileMenu = document.querySelector('#mobile-menu-container');
+
+mobileMenuClick.addEventListener('click', () => {
+  mobileMenu.classList.add('active');
+});
+
+document.addEventListener('click', function (evt) {
+  let targetElement = evt.target;
+
+  if (
+    targetElement !== mobileMenuClick &&
+    targetElement !== mobileMenuIconClick &&
+    targetElement !== mobileMenu
+  ) {
+    mobileMenu.classList.remove('active');
+  }
+
+  // Go up the DOM
+  targetElement = targetElement.parentNode;
 });
